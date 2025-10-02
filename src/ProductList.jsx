412762990cb5ef2a56119077b59e2795e0aa56ce
toniguarisco <toniguarisco@gmail.com>
2025-10-02 17:35:02 -0400

    Fixing cart toggle

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
-import CartItems from './CartItem';
+import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 import { useDispatch } from 'react-redux';
 
